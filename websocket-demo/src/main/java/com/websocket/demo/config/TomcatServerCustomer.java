868735c3ad4459577d1f86e4c5991929d3b24bfd
Maynor Moe <maynormoe0@gmail.com>
2024-05-29 22:32:17 +0800

    feat: 证书更新

--- websocket-demo/src/main/java/com/websocket/demo/config/TomcatServerCustomer.java
+++ websocket-demo/src/main/java/com/websocket/demo/config/TomcatServerCustomer.java
@@ -1,2 +1,20 @@
-package com.websocket.demo.config;public class TomcatServerCustomer {
+package com.websocket.demo.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class TomcatServerCustomer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Value("${server.http-port}")
+    private Integer httpPort;
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        factory.addAdditionalTomcatConnectors(connector);
+    }
 }
