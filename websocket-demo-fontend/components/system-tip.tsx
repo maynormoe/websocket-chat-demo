05a0dd2a7986977ff85910f9386e012f0cc69c67
Maynor Moe <maynormoe0@gmail.com>
2024-05-29 16:15:05 +0800

    feat: 保存聊天记录功能

--- websocket-demo-fontend/components/system-tip.tsx
+++ websocket-demo-fontend/components/system-tip.tsx
@@ -11,7 +11,7 @@ const SystemTip: FC<SystemTipProps> = (props) => {
   const { children, message = "" } = props;
 
   return (
-    <div className="w-full flex justify-center items-center my-2">
+    <div className="w-full flex justify-center items-center my-2 px-2">
       <div className=" w-96  py-1 border-default-200 dark:border-default-100 bg-default-200/20 flex justify-center items-center rounded-md shadow-s ">
         <span className=" text-xs light:text-gray-300">{message}</span>
       </div>
